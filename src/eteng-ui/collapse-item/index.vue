7b8974dc29fa16b54bac93914f1fa3a9ddc1720a
fansonly <251115151@qq.com>
2022-08-25 22:45:58 +0800

    refactor: update collapse

--- src/eteng-ui/collapse-item/index.vue
+++ src/eteng-ui/collapse-item/index.vue
@@ -18,107 +18,109 @@
 </template>
 
 <script>
-  import { getRect } from '@/shared/platform'
-  import { getParentInstance } from '@/shared'
-  export default {
-    name: 'et-collapse-item',
-    props: {
-      // 面板的值属性
-      name: null,
-      // 左侧标题 - 透传
-      title: null,
-      // 右侧文本内容 - 透传
-      text: null,
-      // 左侧标题宽度，不设置默认为铺满 - 透传
-      titleWidth: {
-        type: [String, Number],
-        default: 80
-      },
-      // 是否链接模式，为 true 时显示右侧图标 - 透传
-      isLink: {
-        type: Boolean,
-        default: true
-      },
-      // 右侧图标
-      icon: {
-        type: String,
-        default: 'arrow-down'
-      },
-      // 是否显示标题下边框 - 透传
-      border: {
-        type: Boolean,
-        default: true
-      },
-      // 是否禁用当前面板 - 透传
-      disabled: {
-        type: Boolean,
-        default: false
-      },
-      // 自定义组件class - 透传
-      customClass: null,
-      // 自定义组件样式 - 透传
-      customStyle: null,
-      // 自定义标题样式 - 透传
-      titleStyle: null,
-      // 自定义右侧文本样式 - 透传
-      textStyle: null
+import { getRect } from '@/shared/platform'
+
+export default {
+  name: 'et-collapse-item',
+  inject: ['collapse'],
+  props: {
+    // 面板的值属性
+    name: null,
+    // 左侧标题 - 透传
+    title: null,
+    // 右侧文本内容 - 透传
+    text: null,
+    // 左侧标题宽度，不设置默认为铺满 - 透传
+    titleWidth: {
+      type: [String, Number],
+      default: 80
     },
-    data() {
-      return {
-        animation: false,
-        expanded: false,
-        height: 0,
-      }
+    // 是否链接模式，为 true 时显示右侧图标 - 透传
+    isLink: {
+      type: Boolean,
+      default: true
     },
-    computed: {
-      bodyStyled({ expanded, animation, height }) {
-        let style = ''
-        if (animation) {
-          style += 'transition: height ease 0.3s;'
-        }
-        // Bug: 安卓下 rpx 转换有误差
-        style += `height: ${expanded ? height : 0}px;`
-        return style
-      },
+    // 右侧图标
+    icon: {
+      type: String,
+      default: 'arrow-down'
     },
-    watch: {
-      expanded: {
-        handler(val) {
-          val && this.getBodyHeight()
-        },
-        immediate: true
-      }
+    // 是否显示标题下边框 - 透传
+    border: {
+      type: Boolean,
+      default: true
     },
-    methods: {
-      initRender(val, firstInit = false) {
-        this.animation = !firstInit
-        if (Array.isArray(val)) {
-          this.expanded = val.includes(this.name)
-        } else {
-          if (typeof val === 'boolean') {
-            this.expanded = val
-          } else {
-            this.expanded = this.name === val
-          }
-        }
-      },
-      onClick() {
-        if (this.disabled) return
-        const parent = this.getCollapse()
-        parent && parent.change({name: this.name, expanded: !this.expanded})
-      },
-      getBodyHeight() {
-        this.$nextTick(async () => {
-          const rect = await getRect(this, '.et-collapse-item-content')
-          this.height = rect.height
-        })
-      },
-      getCollapse() {
-        return getParentInstance(this, 'useCollapse')
-      },
-      useCollapseItem() {}
+    // 是否禁用当前面板 - 透传
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    // 自定义组件class - 透传
+    customClass: null,
+    // 自定义组件样式 - 透传
+    customStyle: null,
+    // 自定义标题样式 - 透传
+    titleStyle: null,
+    // 自定义右侧文本样式 - 透传
+    textStyle: null
+  },
+  data() {
+    return {
+      animation: false,
+      expanded: false,
+      height: 0,
     }
+  },
+  computed: {
+    bodyStyled({ expanded, animation, height }) {
+      let style = ''
+      if (animation) {
+        style += 'transition: height ease 0.3s;'
+      }
+      // Bug: 安卓下 rpx 转换有误差
+      style += `height: ${expanded ? height : 0}px;`
+      return style
+    },
+  },
+  watch: {
+    expanded: {
+      handler(val) {
+        val && this.getBodyHeight()
+      },
+      immediate: true
+    }
+  },
+  created() {
+    this.collapse.children.push(this)
+  },
+  mounted() {
+    if (!this.collapse) return
+  },
+  methods: {
+    initRender(val, firstInit = false) {
+      this.animation = !firstInit
+      if (Array.isArray(val)) {
+        this.expanded = val.includes(this.name)
+      } else {
+        if (typeof val === 'boolean') {
+          this.expanded = val
+        } else {
+          this.expanded = this.name === val
+        }
+      }
+    },
+    onClick() {
+      if (this.disabled) return
+      this.collapse.change({name: this.name, expanded: !this.expanded})
+    },
+    getBodyHeight() {
+      this.$nextTick(async () => {
+        const rect = await getRect(this, '.et-collapse-item-content')
+        this.height = rect.height
+      })
+    },
   }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/eteng-ui/collapse/index.vue
+++ src/eteng-ui/collapse/index.vue
@@ -5,70 +5,72 @@
 </template>
 
 <script>
-import { getChildrenCompatibleH5 } from '@/shared'
-  export default {
-    name: 'et-collapse',
-    props: {
-      // 默认展开的值，面板的 name 属性
-      value: null,
-      // 手风琴模式
-      accordion: {
-        type: Boolean,
-        default: false
-      }
-    },
-    data () {
-      return {
-        type: 'string',
-        firstInit: true
-      }
-    },
-    watch: {
-      value: {
-        handler(val) {
-          this.type = typeof val
-          this.update(val, this.firstInit)
-          this.firstInit = false
-        },
-        immediate: true,
-        deep: true
-      }
-    },
-    methods: {
-      change({name, expanded}) {
-        let data = null
-        if (this.accordion) {
-          if (this.type === 'boolean') {
-            data = expanded ? true : false
-          } else {
-            data = expanded ? name : ''
-          }
+export default {
+  name: 'et-collapse',
+  provide() {
+    return {
+      collapse: this
+    }
+  },
+  props: {
+    // 默认展开的值，面板的 name 属性
+    value: null,
+    // 手风琴模式
+    accordion: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      type: 'string',
+      firstInit: true
+    }
+  },
+  watch: {
+    value: {
+      handler(val) {
+        this.type = typeof val
+        this.update(val, this.firstInit)
+        this.firstInit = false
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+  created() {
+    this.children = []
+  },
+  methods: {
+    change({name, expanded}) {
+      let data = null
+      if (this.accordion) {
+        if (this.type === 'boolean') {
+          data = !!expanded
         } else {
-          data = [...this.value]
-          if (expanded) {
-            data.push(name)
-          } else {
-            const index = data.indexOf(name)
-            data.splice(index, 1)
-          }
+          data = expanded ? name : ''
         }
-        this.update(data)
-        // v-model 绑定
-        this.$emit('input', data)
-        this.$emit('change', data)
-      },
-      update(active, firstInit = false) {
-        this.$nextTick(() => {
-          this.getCollapseItem().map(child => {
-            child.initRender(active, firstInit)
-          })
+      } else {
+        data = [...this.value]
+        if (expanded) {
+          data.push(name)
+        } else {
+          const index = data.indexOf(name)
+          data.splice(index, 1)
+        }
+      }
+      this.update(data)
+      // v-model 绑定
+      this.$emit('input', data)
+      this.$emit('change', data)
+    },
+    update(active, firstInit = false) {
+      this.$nextTick(() => {
+        this.children.map(child => {
+          child.initRender(active, firstInit)
         })
-      },
-      getCollapseItem() {
-        const children = getChildrenCompatibleH5(this)
-        return children.filter(child => child['useCollapseItem'])
-      },
-      useCollapse() {}
+      })
     }
   }
+}
 </script>
