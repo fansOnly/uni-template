ea7c07e09d17038b3112e9f9798539ec639d4edc
fansonly <251115151@qq.com>
2022-08-25 22:46:08 +0800

    refactor: update dropdown

--- src/eteng-ui/dropdown-item/index.vue
+++ src/eteng-ui/dropdown-item/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="showWrapper" class="et-dropdown-item-wrapper" :style="wrapperStyled">
-    <et-popup :visible.sync="visible" custom-style="position: absolute;" :duration="duration" overlay-style="position:absolute;" max-height="40vh" position="top" :overlay="overlay" @click-overlay="onClickOverlay" @after-leave="onClosed">
+  <div v-show="showWrapper" class="et-dropdown-item-wrapper" :style="wrapperStyled">
+    <et-popup :visible.sync="visible" custom-style="position: absolute;" :duration="duration" overlay-style="position:absolute;" max-height="40vh" :position="direction === 'down' ? 'top' : 'bottom'" :overlay="overlay" @click-overlay="onClickOverlay" @after-leave="onClosed">
       <et-cell v-for="(item, index) in options" :key="index" :title="item.text" is-link :disabled="item.disabled" :border="index < options.length - 1" @click="onClickItem(item, index)">
         <et-icon v-show="item.value === value" slot="icon" name="selected" size="20" />
       </et-cell>
@@ -9,103 +9,117 @@
 </template>
 
 <script>
-  import { requestAnimationFrame } from '@/shared/platform'
-  import { getParentInstance } from '@/shared'
-  export default {
-    name: 'et-dropdown-item',
-    props: {
-      // 渲染源数据
-      options: {
-        type: Array,
-        default: () => [],
-      },
-      // 默认的选中值，对应下拉菜单的 value 属性
-      value: null,
-      // 禁用当前菜单，不可点击选中
-      disabled: {
-        type: Boolean,
-        default: false,
-      },
+import { requestAnimationFrame } from '@/shared/platform'
+
+export default {
+  name: 'et-dropdown-item',
+  inject: ['dropdown'],
+  props: {
+    // 渲染源数据
+    options: {
+      type: Array,
+      default: () => [],
     },
-    data() {
-      return {
-        overlay: true,
-        closeOnClickOverlay: true,
-        showWrapper: false,
-        visible: false,
-        top: 0,
-        zIndex: 0,
-        duration: 300,
+    // 默认的选中值，对应下拉菜单的 value 属性
+    value: null,
+    // 禁用当前菜单，不可点击选中
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      overlay: true,
+      closeOnClickOverlay: true,
+      showWrapper: false,
+      direction: 'down',
+      visible: false,
+      top: 0,
+      bottom: 0,
+      zIndex: 0,
+      duration: 300,
+    }
+  },
+  computed: {
+    wrapperStyled({ zIndex, top, bottom, direction }) {
+      let style = `z-index: ${zIndex + 1};`
+      if (direction === 'down') {
+        style += `top: ${top}px;`
+      } else {
+        style += `bottom: calc(100% - ${bottom}px);`
+      }
+      return style
+    },
+    activeIndex({ value, options }) {
+      return value ? options.findIndex((v) => v.value === value) : 0
+    },
+  },
+  watch: {
+    value: {
+      handler(val) {
+        this.rerender()
+      },
+      immediate: true,
+    },
+  },
+  created() {
+    this.dropdown.children.push(this)
+  },
+  async mounted() {
+    const { overlay, zIndex, duration, direction, closeOnClickOverlay } = this.dropdown
+    this.overlay = overlay
+    this.zIndex = zIndex
+    this.duration = duration
+    this.direction = direction
+    this.closeOnClickOverlay = closeOnClickOverlay
+  },
+  methods: {
+    async toggle(show, duration = 300) {
+      if (typeof show !== 'boolean') show = !this.visible
+      if (show === this.visible) return
+      this.duration = duration
+      this.visible = show
+      if (show) {
+        // TODO direction = up 时, 如果下拉菜单距顶部的距离小于下拉项的高度，页面布局会错位
+        const { top, bottom } = await this.dropdown.resolveStyle()
+        this.top = top
+        this.bottom = bottom
+        await requestAnimationFrame()
+        this.showWrapper = true
+      }
+      this.rerender()
+    },
+    rerender() {
+      this.$nextTick(() => {
+        this.dropdown && this.dropdown.updateMenu()
+      })
+    },
+    onClickItem(item, index) {
+      if (item.disabled) return
+      this.visible = false
+      this.$emit('close')
+      this.rerender()
+      if (this.value !== item.value) {
+        this.$emit('change', item, this.dropdown.menuIndex)
       }
     },
-    computed: {
-      wrapperStyled({ zIndex, top }) {
-        let style = `z-index: ${zIndex};`
-        style += `top: ${top}px;`
-        return style
-      },
-      activeIndex({ value, options }) {
-        return value ? options.findIndex((v) => v.value === value) : 0
-      },
+    onClickOverlay() {
+      if (!this.closeOnClickOverlay) return
+      this.$emit('close')
+      this.toggle()
     },
-    watch: {
-      value: {
-        handler(val) {
-          this.rerender()
-        },
-        immediate: true,
-      },
+    onClosed() {
+      this.showWrapper = false
     },
-    async mounted() {
-      this.parent = getParentInstance(this, 'useDropdownMenu')
-      const { overlay, zIndex, duration, closeOnClickOverlay } = this.parent
-      this.overlay = overlay
-      this.zIndex = zIndex
-      this.duration = duration
-      this.closeOnClickOverlay = closeOnClickOverlay
-    },
-    methods: {
-      async toggle(show, duration = 300) {
-        if (typeof show !== 'boolean') show = !this.visible
-        if (show === this.visible) return
-        this.visible = show
-        this.duration = duration
-        if (show) {
-          this.top = this.parent.top
-          await requestAnimationFrame()
-          this.showWrapper = true
-        }
-        this.rerender()
-      },
-      rerender() {
-        this.$nextTick(() => {
-          this.parent && this.parent.updateMenu()
-        })
-      },
-      onClickItem(item, index) {
-        if (item.disabled) return
-        this.visible = false
-        this.$emit('close')
-        this.rerender()
-        if (this.value !== item.value)
-          this.$emit('change', item, this.parent.menuIndex)
-      },
-      onClickOverlay() {
-        if (!this.closeOnClickOverlay) return
-        this.$emit('close')
-        this.toggle()
-      },
-      onClosed() {
-        this.showWrapper = false
-      },
-      useDropdownItem() {}
-    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
   .et-dropdown-item-wrapper {
     position: fixed;
+    top: 0;
     right: 0;
     bottom: 0;
     left: 0;
--- src/eteng-ui/dropdown-menu/index.vue
+++ src/eteng-ui/dropdown-menu/index.vue
@@ -11,104 +11,109 @@
 </template>
 
 <script>
-  import { getRect, requestAnimationFrame } from '@/shared/platform'
-  import cssVariables from '@/shared/css-variables'
-  import { getChildrenCompatibleH5 } from '@/shared'
+import { getRect, requestAnimationFrame } from '@/shared/platform'
+import cssVariables from '@/shared/css-variables'
 
-  export default {
-    name: 'et-dropdown-menu',
-    props: {
-      // 高亮色值
-      activeColor:{
-        type: String,
-        default: cssVariables.primaryColor
-      },
-      // 弹窗动画时长 ms - 透传
-      duration: {
-        type: Number,
-        default: 300
-      },
-      // 弹窗层级 - 透传
-      zIndex: {
-        type: Number,
-        default: 100
-      },
-      // 是否显示遮罩层 - 透传
-      overlay: {
-        type: Boolean,
-        default: true
-      },
-      // 点击遮罩层是否关闭 - 透传
-      closeOnClickOverlay: {
-        type: Boolean,
-        default: true
-      },
-      // 自定义组件样式
-      customStyle: null
+export default {
+  name: 'et-dropdown-menu',
+  provide() {
+    return {
+      dropdown: this
+    }
+  },
+  props: {
+    // 高亮色值
+    activeColor: {
+      type: String,
+      default: cssVariables.primaryColor
     },
-    data() {
-      return {
-        menus: [], // 菜单项
-        menuIndex: -1,
-        top: 0, // 下拉菜单定位高度
+    // 方向
+    direction: {
+      type: String,
+      default: 'down'
+    },
+    // 弹窗动画时长 ms - 透传
+    duration: {
+      type: Number,
+      default: 300
+    },
+    // 弹窗层级 - 透传
+    zIndex: {
+      type: Number,
+      default: 100
+    },
+    // 是否显示遮罩层 - 透传
+    overlay: {
+      type: Boolean,
+      default: true
+    },
+    // 点击遮罩层是否关闭 - 透传
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    // 自定义组件样式
+    customStyle: null
+  },
+  data() {
+    return {
+      menus: [], // 菜单项
+      menuIndex: -1,
+    }
+  },
+  created() {
+    this.children = []
+  },
+  async mounted() {
+    this.updateMenu()
+  },
+  methods: {
+    updateMenu() {
+      this.menus = this.children.map((child, index) => {
+        return { id: index, value: child.value, active: child.visible, options: child.options, disabled: child.disabled }
+      })
+    },
+    genMenuName({options = [], value = '', title = ''}) {
+      if (title) return title
+      const menu = options.find(v => v.value === value)
+      return (menu && menu.text) || options[0].text
+    },
+    toggleItem(active = -1) {
+      this.children.map((child, index) => {
+        if (active === index) {
+          child.toggle()
+        } else if (child.visible) {
+          child.toggle(false, 60 /** duration */)
+        }
+      })
+    },
+    close(duration = 60) {
+      this.children.map(child => {
+        child.toggle(false, duration)
+      })
+    },
+    async onClickMenu(item, index) {
+      if (item.disabled) return
+      if (this.menuIndex === index && item.active) {
+        this.close(300)
+      } else {
+        this.close()
+        await requestAnimationFrame()
+        this.menuIndex = index
+        this.toggleItem(index)
       }
     },
-    async mounted() {
-      this.updateMenu()
-      await requestAnimationFrame()
-      await this.resolveStyle()
+    async resolveStyle() {
+      const rect = await getRect(this, '.et-dropdown-wrapper')
+      let top = rect.bottom
+      /* #ifdef H5 */
+      // fix：H5 下，uni-app 会默认生成一个头部 uni-page-head，位置计算会有误差
+      top += 44
+      /* #endif */
+      return { top, bottom: rect.top }
     },
-    methods: {
-      updateMenu() {
-        this.menus = this.getDropdownItems().map((child, index) => {
-          return { id: index, value: child.value, active: child.visible, options: child.options, disabled: child.disabled }
-        })
-      },
-      genMenuName({options = [], value = '', title = ''}) {
-        if (title) return title
-        const menu = options.find(v => v.value === value)
-        return (menu && menu.text) || options[0].text
-      },
-      toggleItem(active = -1) {
-        this.getDropdownItems().map((child, index) => {
-          if (active === index) {
-            child.toggle()
-          } else if (child.visible) {
-            child.toggle(false, 60 /** duration */)
-          }
-        })
-      },
-      close(duration = 60) {
-        this.getDropdownItems().map(child => {
-          child.toggle(false, duration)
-        })
-      },
-      getDropdownItems() {
-        const children = getChildrenCompatibleH5(this)
-        return children.filter(child => child['useDropdownItem'])
-      },
-      async onClickMenu(item, index) {
-        if (item.disabled) return
-        if (this.menuIndex === index && item.active) {
-          this.close(300)
-        } else {
-          this.close()
-          await requestAnimationFrame()
-          this.menuIndex = index
-          this.toggleItem(index)
-        }
-      },
-      async resolveStyle() {
-        const rect = await getRect(this, '.et-dropdown-wrapper')
-        /* #ifdef H5 */
-        // fix：H5 下，uni-app 会默认生成一个头部 uni-page-head，位置计算会有误差
-        this.top += 44
-        /* #endif */
-        this.top += rect.bottom
-      },
-      useDropdownMenu() {}
-    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
