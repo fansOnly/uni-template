3a3a4fcf87da4562daf58e4dce412c77814845ec
fansoly <251115151@qq.com>
2022-11-29 10:51:40 +0800

    fix: 代码优化

--- src/vitamincc-ui/cell/index.vue
+++ src/vitamincc-ui/cell/index.vue
@@ -1,17 +1,21 @@
 <template>
-  <view :class="['vc-cell', `vc-cell-${size}`, isBorder ? 'vc-hairline--bottom' : null, disabled ? 'vc-cell--disabled' : null, customClasses]" :style="wrapperStyled" :hover-class="cellHoverClass" :hover-stay-time="200" @click="onClick">
+  <view
+    :class="['vc-cell', `vc-cell-${size}`, isBorder ? 'vc-hairline--bottom' : null, disabled ? 'vc-cell--disabled' : null, customClasses]"
+    :style="wrapperStyled" :hover-class="cellHoverClass" :hover-stay-time="200" @click="onClick">
     <view :class="['vc-cell__title', titleClasses]" :style="titleStyled">
-      <template v-if="title">{{title}}</template>
+      <template v-if="title">{{ title }}</template>
       <slot v-else name="title"></slot>
     </view>
     <view class="vc-cell__right" :style="rightStyled">
       <view :class="['vc-cell__value', textClasses]" :style="textStyled">
-        <template v-if="text">{{text}}</template>
+        <template v-if="text">{{ text }}</template>
         <slot v-else name="text"></slot>
       </view>
       <view v-if="isLink" class="vc-cell__right-icon">
         <!-- slot 命名 right-icon 不支持？？？ -->
-        <slot name="icon"><vc-icon name="arrow-right" /></slot>
+        <slot name="icon">
+          <vc-icon name="arrow-right" />
+        </slot>
       </view>
       <slot name="extra"></slot>
     </view>
@@ -19,7 +23,7 @@
 </template>
 
 <script>
-import { addUnit } from '../common/util';
+import { addUnit } from '../common/util'
 
 export default {
   name: 'vc-cell',
@@ -54,7 +58,7 @@ export default {
       type: String,
       default: 'normal',
       validator(value) {
-        return ['normal', 'small', 'mini', 'large'].includes(value);
+        return ['normal', 'small', 'mini', 'large'].includes(value)
       }
     },
     // 当 isLink 为 true 可设置高亮颜色
@@ -73,7 +77,7 @@ export default {
     },
     // 微信 view 标签的 hover-class
     hoverClass: {
-      type: [String, null],
+      type: String,
       default: 'vc-cell-hover-class'
     },
     // 自定义组件类 class
@@ -91,100 +95,110 @@ export default {
   },
   computed: {
     wrapperStyled() {
-      let style = '';
-      const lineHeight = this.height === 'auto' ? 'auto' : this.height + 'px';
-      style += `height: ${lineHeight};`;
-      return style + (this.customStyle ?? '') + (this.parent?.customStyle ?? '');
+      let style = ''
+      const lineHeight = this.height === 'auto' ? 'auto' : this.height + 'px'
+      style += `height: ${lineHeight};`
+      return style + (this.customStyle ?? '') + (this.parent?.customStyle ?? '')
     },
     titleStyled() {
-      let style = '';
+      let style = ''
       if (this.titleWidth) {
-        style += `min-width: ${addUnit(this.titleWidth)};`;
+        style += `min-width: ${addUnit(this.titleWidth)};`
       } else {
-        style += 'flex: 1;';
+        style += 'flex: 1;'
       }
-      return style + (this.titleStyle ?? '') + (this.parent?.titleStyle ?? '');
+      return style + (this.titleStyle ?? '') + (this.parent?.titleStyle ?? '')
     },
     rightStyled() {
-      return Number(this.titleWidth) === 0 ? '' : 'flex: 1;';
+      return Number(this.titleWidth) === 0 ? '' : 'flex: 1;'
     },
     textStyled() {
-      let style = '';
+      let style = ''
       if (this.isLinkActive && this.linkActiveColor) {
-        style += `color: ${this.linkActiveColor};`;
+        style += `color: ${this.linkActiveColor};`
       }
-      return style + (this.textStyle ?? '') + (this.parent?.textStyle ?? '');
+      return style + (this.textStyle ?? '') + (this.parent?.textStyle ?? '')
     },
     customClasses() {
-      return this.customClass + (this.parent?.customClass ?? '');
+      return this.customClass + (this.parent?.customClass ?? '')
     },
     titleClasses() {
-      return this.titleClass + (this.parent?.titleClass ?? '');
+      return this.titleClass + (this.parent?.titleClass ?? '')
     },
     textClasses() {
-      return this.textClass + (this.parent?.textClass ?? '');
+      return this.textClass + (this.parent?.textClass ?? '')
     },
     isBorder() {
-      return this.border === null ? this.parent?.border : this.border;
+      return this.border === null ? this.parent?.border : this.border
     },
     cellHoverClass() {
-      return this.isLink ? this.hoverClass : null;
+      return this.isLink ? this.hoverClass : 'none'
     }
   },
   methods: {
     onClick(evt) {
-      if (this.disabled) return;
-      this.$emit('click', evt);
+      if (this.disabled) return
+      this.$emit('click', evt)
     },
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
-  .vc-cell {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    box-sizing: border-box;
-    background: #fff;
-    overflow: hidden;
-    &-normal {
-      padding: 15px 32rpx;
-    }
-    &-small {
-      padding: 13px 32rpx;
-    }
-    &-mini {
-      padding: 6px 32rpx;
-    }
-    &-large {
-      padding: 18px 32rpx;
-    }
+.vc-cell {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  background: #fff;
+  overflow: hidden;
+
+  &-normal {
+    padding: 15px 32rpx;
   }
-  .vc-cell--disabled .vc-cell__title,
-  .vc-cell--disabled .vc-cell__value {
-    color: $uni-text-color-disabled!important;
-    /* #ifdef H5 */
-    cursor: not-allowed;
-    /* #endif */
+
+  &-small {
+    padding: 13px 32rpx;
   }
-  .vc-cell--disabled .vc-cell__right-icon {
-    opacity: .6;
+
+  &-mini {
+    padding: 6px 32rpx;
   }
-  .vc-cell__title {
-    text-align: left;
+
+  &-large {
+    padding: 18px 32rpx;
   }
-  .vc-cell__right {
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-  }
-  .vc-cell__value {
-    flex: 1;
-    color: $uni-text-color-sub;
-    text-align: right;
-  }
-  .vc-cell-hover-class {
-    background: #fafafa;
-  }
+}
+
+.vc-cell--disabled .vc-cell__title,
+.vc-cell--disabled .vc-cell__value {
+  color: $uni-text-color-disabled !important;
+  /* #ifdef H5 */
+  cursor: not-allowed;
+  /* #endif */
+}
+
+.vc-cell--disabled .vc-cell__right-icon {
+  opacity: .6;
+}
+
+.vc-cell__title {
+  text-align: left;
+}
+
+.vc-cell__right {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.vc-cell__value {
+  flex: 1;
+  color: $uni-text-color-sub;
+  text-align: right;
+}
+
+.vc-cell-hover-class {
+  background: #fafafa;
+}
 </style>
