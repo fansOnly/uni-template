46af774616e5d3d31017a20d73424f1ce2fd2fb5
fansoly <251115151@qq.com>
2022-12-01 11:22:05 +0800

    chore: delete files

--- build/utils/path.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const filePath = fileURLToPath(import.meta.url)
-const dirPath = path.dirname(filePath)
-
-export const projRoot = path.resolve(dirPath, '../../')
-
-export const resolvePath = (file) => path.resolve(projRoot, file)
