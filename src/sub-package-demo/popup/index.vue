9ac4329047316036e92c6d7f0b581a964301de78
fansoly <251115151@qq.com>
2023-01-10 14:15:28 +0800

    feat: popup for scss

--- src/sub-package-demo/popup/index.vue
+++ src/sub-package-demo/popup/index.vue
@@ -2,24 +2,28 @@
   <view class="page-wrapper" style="height: 2000px">
 
     <demo-block title="基础用法 - 锁定滚动穿透" padding>
-      <vc-cell title="弹窗 - 淡入" is-link border @click="(visible4 = true)"></vc-cell>
-      <vc-cell title="弹窗 - 顶部弹出" is-link border @click="(position = 'top', visible = true)"></vc-cell>
-      <vc-cell title="弹窗 - 左侧弹出" is-link border @click="(position = 'left', visible = true)"></vc-cell>
-      <vc-cell title="弹窗 - 右侧弹出" is-link border @click="(position = 'right', visible = true)"></vc-cell>
-      <vc-cell title="弹窗 - 底部弹出" is-link @click="(position = 'bottom', visible = true)"></vc-cell>
+      <vc-cell-group border>
+        <vc-cell title="弹窗 - 淡入" is-link @click="(visible4 = true)"></vc-cell>
+        <vc-cell title="弹窗 - 顶部弹出" is-link @click="(position = 'top', visible = true)"></vc-cell>
+        <vc-cell title="弹窗 - 左侧弹出" is-link @click="(position = 'left', visible = true)"></vc-cell>
+        <vc-cell title="弹窗 - 右侧弹出" is-link @click="(position = 'right', visible = true)"></vc-cell>
+        <vc-cell title="弹窗 - 底部弹出" is-link is-last @click="(position = 'bottom', visible = true)"></vc-cell>
+      </vc-cell-group>
     </demo-block>
 
     <demo-block title="进阶用法" padding>
-      <vc-cell title="弹窗 - 底部 - 圆角弹窗" is-link border @click="(position = 'bottom', visible2 = true)"></vc-cell>
-      <vc-cell title="弹窗 - 底部 - 携带标题" is-link border @click="visible3 = true"></vc-cell>
-      <vc-cell title="弹窗 - 居中 - 携带标题" is-link @click="visible5 = true"></vc-cell>
+      <vc-cell-group border>
+        <vc-cell title="弹窗 - 底部 - 左关闭" is-link @click="visible3 = true, closePosition = 'left'"></vc-cell>
+        <vc-cell title="弹窗 - 底部 - 右关闭" is-link @click="visible3 = true, closePosition = 'right'"></vc-cell>
+        <vc-cell title="弹窗 - 居中 - 携带标题" is-link is-last @click="visible5 = true"></vc-cell>
+      </vc-cell-group>
     </demo-block>
 
     <vc-popup :visible.sync="visible4" position="center">
       <view class="box"></view>
     </vc-popup>
 
-    <vc-popup :visible.sync="visible5" title="暂时只支持绑定下列银行" position="center" round border>
+    <vc-popup :visible.sync="visible5" title="暂时只支持绑定下列银行" position="center" round>
       <view class="bank-popup-wrapper">
         <view class="">这里是银行卡列表</view>
         <view class="">这里是银行卡列表</view>
@@ -37,11 +41,8 @@
       <view style="line-height: 100px;">这里是银行卡列表66</view>
       <view style="line-height: 100px;">这里是银行卡列表77</view>
     </vc-popup>
-    <vc-popup :visible.sync="visible2" :position="position" round>
-      <scroll-view scroll-y :style="style">
-      </scroll-view>
-    </vc-popup>
-    <vc-popup :visible.sync="visible3" position="bottom" round title="标题" closeable>
+    <vc-popup :visible.sync="visible3" position="bottom" round title="标题" closeable
+      :close-icon-position="closePosition">
       <view class="content">
         1、中国税收居民是指在中国境内有住所，或者无住所而在境内居住满一年的个人。在中国境内有住所是指因户籍、家庭、经济利益关系而在中国境内习惯性居住。在境内居住满一年，是指在一个纳税年度中在中国境内居住365日。临时离境的，不扣减日数。临时离境，是指在一个纳税年度中一次不超过30日或者多次累计不超过90日的离境。
         2、非中国税收居民是指中国税收居民以外的个人。</view>
@@ -58,11 +59,11 @@ export default {
   data() {
     return {
       visible: false,
-      visible2: false,
       visible3: false,
       visible4: false,
       visible5: false,
       position: 'center',
+      closePosition: 'left'
     }
   },
   computed: {
--- src/vitamincc-ui/popup/index.vue
+++ src/vitamincc-ui/popup/index.vue
@@ -1,19 +1,19 @@
 <template>
   <view @touchmove.stop="noop">
     <view v-if="display"
-      :class="['vc-popup', `vc-popup--${position}`, round ? 'vc-popup--round' : null, safeAreaInsetTop ? 'vc-popup--top--safe' : null, safeAreaInsetBottom ? 'vc-popup--bottom--safe' : null, classes]"
+      :class="['vc-popup', `is-${position}`, round ? 'is-round' : null, safeAreaInsetTop ? 'is-top-safe' : null, safeAreaInsetBottom ? 'is-bottom--safe' : null, classes]"
       :style="styled" @transitionend="onTransitionEnd">
       <slot name="header">
-        <view v-if="title && title.length" :class="['vc-popup-header', border ? 'vc-hairline--bottom' : null]">
-          <view class="vc-popup-header-title">{{ title }}</view>
+        <view v-if="title && title.length" :class="['vc-popup__header', border ? 'vc-hairline--bottom' : null]">
+          <view class="vc-popup__title">{{ title }}</view>
         </view>
       </slot>
-      <view v-if="closeable" :class="['vc-popup-header-close', 'vc-popup-close--' + closeIconPosition]" @click="close">
+      <view v-if="closeable" :class="['vc-popup__close', 'vc-popup__close--' + closeIconPosition]" @click="close">
         <slot name="close">
-          <vc-icon name="cross-blank" />
+          <vc-icon name="cross-blank" :size="20" color="#909399" />
         </slot>
       </view>
-      <scroll-view scroll-y class="vc-popup-body" :style="bodyStyled">
+      <scroll-view scroll-y class="vc-popup__body" :style="bodyStyled">
         <slot />
       </scroll-view>
     </view>
@@ -24,7 +24,6 @@
 
 <script>
 import transition from '../mixins/transition'
-import cssVariables from '@/common/theme'
 import { addUnit } from '../common/util'
 import { getAppData } from '../common/global-data'
 
@@ -44,11 +43,6 @@ export default {
     title: null,
     // 弹窗Y轴位移
     offset: {
-      type: [String, Number],
-      default: 0
-    },
-    // 顶部距离
-    bottom: {
       type: [String, Number],
       default: 0
     },
@@ -74,7 +68,7 @@ export default {
     // 是否显示标题下边框线
     border: {
       type: Boolean,
-      default: false
+      default: true
     },
     // 是否圆角
     round: {
@@ -104,7 +98,7 @@ export default {
     // 弹窗层级
     zIndex: {
       type: Number,
-      default: +cssVariables.popupZIndex
+      default: 9000
     },
     // 是否为 iPhoneX 留出底部安全距离
     safeAreaInsetBottom: {
@@ -117,11 +111,11 @@ export default {
       default: false
     },
     // 自定义组件样式
-    customStyle: null,
+    customStyle: String,
     // 自定义内容样式
-    bodyStyle: null,
+    bodyStyle: String,
     // 自定义遮罩层样式
-    overlayStyle: null
+    overlayStyle: String
   },
   computed: {
     animationName({ position }) {
@@ -138,11 +132,10 @@ export default {
           return 'fade'
       }
     },
-    styled({ offset, bottom, zIndex, currentDuration, display, customStyle }) {
+    styled({ offset, zIndex, currentDuration, display, customStyle }) {
       const [isCustomNavigation, navHeight] = getAppData(['isCustomNavigation', 'navHeight'])
       let style = `z-index: ${zIndex};`
       style += `margin-top: calc(${offset}px + ${isCustomNavigation ? navHeight : 0}px);`
-      if (bottom) style += `margin-bottom: ${bottom}px;`
       style += `transition-duration: ${currentDuration}ms;`
       if (!display) style += 'display: none;'
       return style + customStyle
@@ -181,107 +174,5 @@ export default {
 
 <style lang="scss" scoped>
 @import "../common/style/transition.scss";
-
-.vc-popup {
-  position: fixed;
-  box-sizing: border-box;
-  overflow-y: auto;
-  -webkit-overflow-scrolling: touch;
-  animation-timing-function: ease;
-  animation: ease both;
-  background-color: #fff;
-}
-
-.vc-popup--center {
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
-
-  &.vc-popup--round {
-    border-radius: 20rpx;
-  }
-}
-
-.vc-popup--top {
-  top: 0;
-  left: 0;
-  width: 100%;
-
-  &.vc-popup--round {
-    border-radius: 0 0 20rpx 20rpx;
-  }
-}
-
-.vc-popup--bottom {
-  left: 0;
-  bottom: 0;
-  width: 100%;
-
-  &.vc-popup--round {
-    border-radius: 20rpx 20rpx 0 0;
-  }
-}
-
-.vc-popup--bottom.vc-popup--bottom--safe {
-  padding-bottom: env(safe-area-inset-bottom);
-}
-
-.vc-popup--top.vc-popup--top--safe {
-  padding-bottom: env(safe-area-inset-top);
-}
-
-.vc-popup--left {
-  top: 0;
-  bottom: 0;
-  left: 0;
-
-  // transform: translate3d(0,-50%,0);
-  &.vc-popup--round {
-    border-radius: 0 20rpx 20rpx 0;
-  }
-}
-
-.vc-popup--right {
-  top: 0;
-  right: 0;
-  bottom: 0;
-
-  // transform: translate3d(0,-50%,0);
-  &.vc-popup--round {
-    border-radius: 20rpx 0 0 20rpx;
-  }
-}
-
-.vc-popup-header {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  height: 112rpx;
-  padding: 0 32rpx;
-  /* color: $uni-text-color; */
-  text-align: center;
-}
-
-.vc-popup-header-close {
-  position: absolute;
-  /* top: 50%; */
-  top: 56rpx;
-  transform: translateY(-50%);
-  z-index: 2;
-}
-
-.vc-popup-close--left {
-  left: $uni-spacing-12
-}
-
-.vc-popup-close--right {
-  right: $uni-spacing-12
-}
-
-.vc-popup-body {
-  -webkit-overflow-scrolling: touch;
-  overflow-y: auto;
-  overscroll-behavior: none;
-}
+@import '../theme-chalk/components/popup.scss';
 </style>
