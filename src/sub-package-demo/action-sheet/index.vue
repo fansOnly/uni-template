88e432ac886914d6262d2294f91f6e0448dead6c
fansoly <251115151@qq.com>
2023-01-12 16:33:18 +0800

    feat: action sheet for scss

--- src/sub-package-demo/action-sheet/index.vue
+++ src/sub-package-demo/action-sheet/index.vue
@@ -1,18 +1,24 @@
 <template>
   <view class="page-wrapper">
     <demo-block title="基础用法" padding>
-      <vc-cell title="基础用法" is-link border @click="visible = true"></vc-cell>
-      <vc-cell title="取消按钮" is-link border @click="visible2 = true"></vc-cell>
-      <vc-cell title="禁用选项" is-link border @click="visible3 = true"></vc-cell>
-      <vc-cell title="圆角" is-link @click="visible4 = true"></vc-cell>
+      <vc-cell-group border>
+        <vc-cell title="基础用法" is-link @click="visible = true"></vc-cell>
+        <vc-cell title="圆角" is-link @click="visible4 = true"></vc-cell>
+        <vc-cell title="自定义高亮颜色" is-link @click="visible5 = true"></vc-cell>
+        <vc-cell title="取消按钮" is-link @click="visible2 = true"></vc-cell>
+        <vc-cell title="禁用选项" is-link is-last @click="visible3 = true"></vc-cell>
+      </vc-cell-group>
     </demo-block>
 
-    <vc-action-sheet :visible.sync="visible" :value="value" :data="list" @select="onSelect"
+    <vc-action-sheet :visible.sync="visible" :value="value" :options="list" @select="onSelect"
       @close="onClose"></vc-action-sheet>
-    <vc-action-sheet :visible.sync="visible2" :value="value2" show-cancel :data="list" @select="onSelect2"
+    <vc-action-sheet :visible.sync="visible5" :value="value" :options="list" color="#f60" @select="onSelect"
+      @close="onClose"></vc-action-sheet>
+    <vc-action-sheet :visible.sync="visible2" :value="value2" show-cancel :options="list" @select="onSelect2"
       @cancel="onCancel"></vc-action-sheet>
-    <vc-action-sheet :visible.sync="visible3" :value="value3" :data="list2" @select="onSelect3"></vc-action-sheet>
-    <vc-action-sheet :visible.sync="visible4" :value="value3" :data="list2" round @select="onSelect3"></vc-action-sheet>
+    <vc-action-sheet :visible.sync="visible3" :value="value3" :options="list2" @select="onSelect3"></vc-action-sheet>
+    <vc-action-sheet :visible.sync="visible4" :value="value3" :options="list" round
+      @select="onSelect3"></vc-action-sheet>
   </view>
 </template>
 
@@ -25,7 +31,7 @@ export default {
   data() {
     return {
       list: [
-        { title: '选项一', value: '1' },
+        { title: '选项一', value: '1', icon: 'search' },
         { title: '选项二', value: '2' },
         { title: '选项三', value: '3' },
       ],
@@ -38,6 +44,7 @@ export default {
       visible2: false,
       visible3: false,
       visible4: false,
+      visible5: false,
       value: '2',
       value2: '',
       value3: '',
@@ -72,5 +79,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/common/demo.scss';
+
 </style>
--- src/vitamincc-ui/action-sheet/index.vue
+++ src/vitamincc-ui/action-sheet/index.vue
@@ -1,27 +1,24 @@
 <template>
   <vc-popup :visible="visible" position="bottom" :round="round" :duration="duration"
     :close-on-click-overlay="closeOnClickOverlay" @click-overlay="clickOverlay">
-    <view :class="['vc-popup-container', round ? 'vc-popup--round' : null]" :style="styled">
-      <view v-for="(item, index) in data" :key="index" class="vc-action-sheet-item vc-hairline--bottom"
-        @click="onSelect(item)">
-        <view :class="['vc-action-sheet--flex', item.disabled ? 'vc-action-sheet--disabled' : 'tap-effect']">
-          <view class="vc-action-sheet-info">
-            <view class="vc-action-sheet__title" :style="{ 'color': value === item[valueProp] ? color : 'inherit' }">
-              {{ item[textProp] }}</view>
-          </view>
-        </view>
+    <view :class="['vc-action-sheet', round ? 'is-round' : null]" :style="customStyle">
+      <view v-for="(item, index) in options" :key="index"
+        :class="['vc-action-sheet__item', index < options.length - 1 ? 'vc-hairline--bottom' : null, value === item[valueProp] ? 'is-active' : null, item.disabled ? 'is-disabled' : null]"
+        :style="{ 'color': value === item[valueProp] && color ? color : '' }" @click="onClickItem(item)">
+        <vc-icon v-if="item.icon" :name="item.icon" custom-style="margin-right: 6px;" />
+        <text class="vc-action-sheet__text">{{ item[textProp] }}</text>
       </view>
-      <template v-if="showCancel">
-        <view class="vc-action-sheet__gap"></view>
-        <vc-button size="large" :border="false" plain block color="#737373" @click="cancel">{{ cancelText }}</vc-button>
-      </template>
     </view>
+    <template v-if="showCancel">
+      <view class="vc-action-sheet__gap"></view>
+      <view class="vc-action-sheet__cancel" hover-class="vc-action-sheet__cancel--hover" @click="onCancel">
+        {{ cancelText }}
+      </view>
+    </template>
   </vc-popup>
 </template>
 
 <script>
-import { addUnit } from '../common/util'
-import cssVariables from '@/common/theme'
 export default {
   name: 'vc-action-sheet',
   props: {
@@ -31,31 +28,40 @@ export default {
       default: false,
     },
     // 渲染源数据
-    data: {
+    options: {
       type: Array,
       default: () => []
     },
     // 激活的值
     value: null,
-    // height 默认自适应，如果内容过多，需要设置高度防止内容过溢出屏幕
-    height: {
-      type: [Number, String],
-      default: 'auto',
-    },
     // 是否显示圆角 - 透传 popup 组件
     round: {
       type: Boolean,
       default: false,
     },
+    // 激活的颜色
+    color: {
+      type: String,
+    },
+    // 文本属性映射
+    textProp: {
+      type: String,
+      default: 'title',
+    },
+    // 值属性映射
+    valueProp: {
+      type: String,
+      default: 'value',
+    },
+    // 值没有变化时是否触发刷新
+    refresh: {
+      type: Boolean,
+      default: false
+    },
     // 动画时长 ms - 透传 popup 组件
     duration: {
       type: Number,
       default: 300,
-    },
-    // 激活的颜色
-    color: {
-      type: String,
-      default: cssVariables.primaryColor
     },
     // 是否显示取消按钮 - 透传 popup 组件
     showCancel: {
@@ -77,34 +83,11 @@ export default {
       type: Boolean,
       default: true,
     },
-    // 文本属性映射
-    textProp: {
-      type: String,
-      default: 'title',
-    },
-    // 值属性映射
-    valueProp: {
-      type: String,
-      default: 'value',
-    },
-    // 值没有变化时是否触发刷新
-    refresh: {
-      type: Boolean,
-      default: false
-    },
     // 自定义组件样式
-    customStyle: null
-  },
-  computed: {
-    styled() {
-      let style = ''
-      style += `height: ${addUnit(this.height)};`
-      style += `color: ${cssVariables.textColor};`
-      return style + this.customStyle
-    },
+    customStyle: String
   },
   methods: {
-    onSelect(item) {
+    onClickItem(item) {
       if (item.disabled) return
       if (this.value === item[this.valueProp] && !this.refresh) return
       this.$emit('select', item)
@@ -114,7 +97,7 @@ export default {
       if (!this.closeOnClickOverlay) return
       this.close()
     },
-    cancel() {
+    onCancel() {
       this.$emit('cancel')
       this.$emit('update:visible', false)
     },
@@ -127,56 +110,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.vc-popup-container {
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  width: 100%;
-  overflow: hidden;
-}
-
-.vc-popup--round {
-  border-top-left-radius: 16rpx;
-  border-top-right-radius: 16rpx;
-}
-
-.vc-popup__body {
-  max-height: 70%;
-}
-
-.vc-action-sheet-item:last-child:after {
-  display: none;
-}
-
-.vc-action-sheet--flex {
-  display: flex;
-  align-items: center;
-  height: 104rpx;
-  padding: 0 32rpx;
-}
-
-.vc-action-sheet--disabled {
-  // opacity: .5;
-  color: $uni-text-color-disabled;
-  cursor: not-allowed;
-}
-
-.vc-action-sheet-info {
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.vc-action-sheet__title {
-  &.is-active {
-    color: $uni-color-primary;
-    /* font-weight: 500; */
-  }
-}
-
-.vc-action-sheet__gap {
-  height: 24rpx;
-  background-color: $uni-bg-grey;
-}
+@import '../theme-chalk/components/action-sheet.scss';
 </style>
