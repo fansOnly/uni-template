3fc7070354ba7c9ddf4a6fdb8b9adc949479d0b5
fansoly <251115151@qq.com>
2023-01-19 15:42:06 +0800

    feat(provide-config): TODO

---
+++ src/vitamincc-ui/config-provider/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'vc-config-provider',
+  props: {
+    vars: Object
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
